7d68be0ba540f05ae315ca536030aaec91503e29
rorp <rorp@protonmail.com>
2023-05-09 21:46:53 -0700

    bump version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   // common
-  lazy val secp256k1KmpJniJvm = "fr.acinq.secp256k1" % "secp256k1-kmp-jni-jvm" % "0.9.0"
+  lazy val secp256k1KmpJniJvm = "fr.acinq.secp256k1" % "secp256k1-kmp-jni-jvm" % "0.7.0"
   lazy val bitcoinKmpJvm = "fr.acinq.bitcoin" % "bitcoin-kmp-jvm" % "0.10.0"
 
   lazy val json4sJackson = "org.json4s" %% "json4s-jackson" % "4.0.3"
@@ -14,7 +14,7 @@ object Dependencies {
   lazy val akkaStreams = "com.typesafe.akka" %% "akka-stream" % "2.6.20"
 
   // test
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.14"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
   lazy val akkaTestKit = "com.typesafe.akka" %% "akka-testkit" % "2.6.20"
   lazy val testContainers = "com.dimafeng" %% "testcontainers-scala-scalatest" % "0.40.12"
 }
